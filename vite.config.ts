8b68b58b57962320598b926637285815907a2782
zinkin69 <marat.medeubayev666@gmail.com>
2026-03-20 13:33:50 +0500

    fix: remove jekyll workflow and set base path to root

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/123/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
